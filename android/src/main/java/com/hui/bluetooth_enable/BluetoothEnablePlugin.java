e417bf3b7ecfd73b17a496f94d73bbc4b8c616be
Rémy Raes <contact@remyraes.com>
2021-09-16 15:20:48 +0200

    [feat] BluetoothEnablePlugin implements ActivityAware

--- android/src/main/java/com/hui/bluetooth_enable/BluetoothEnablePlugin.java
+++ android/src/main/java/com/hui/bluetooth_enable/BluetoothEnablePlugin.java
@@ -9,6 +9,8 @@ import android.content.Intent;
 import android.util.Log;
 import android.content.BroadcastReceiver;
 
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -20,7 +22,7 @@ import io.flutter.plugin.common.PluginRegistry.ActivityResultListener;
 import io.flutter.plugin.common.PluginRegistry;
 
 /** FlutterBluePlugin */
-public class BluetoothEnablePlugin implements FlutterPlugin, MethodCallHandler, ActivityResultListener, PluginRegistry.RequestPermissionsResultListener {
+public class BluetoothEnablePlugin implements FlutterPlugin, ActivityAware, MethodCallHandler, ActivityResultListener, PluginRegistry.RequestPermissionsResultListener {
     private static final String TAG = "BluetoothEnablePlugin";
     private final Registrar registrar;
     private final Activity activity;
@@ -149,5 +151,29 @@ public class BluetoothEnablePlugin implements FlutterPlugin, MethodCallHandler, 
     @Override
     public void onDetachedFromEngine(FlutterPluginBinding binding) {
         // TODO: your plugin is no longer attached to a Flutter experience.
+    }
+
+    @Override
+    public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
+        // TODO: your plugin is now attached to an Activity
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        // TODO: the Activity your plugin was attached to was
+        // destroyed to change configuration.
+        // This call will be followed by onReattachedToActivityForConfigChanges().
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
+        // TODO: your plugin is now attached to a new Activity
+        // after a configuration change.
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        // TODO: your plugin is no longer associated with an Activity.
+        // Clean up references.
     }
 }
